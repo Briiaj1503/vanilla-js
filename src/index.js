8c42b03612d3745f496a5d943a666c0028d5b80d
Britseidy <britseidyaj@gmail.com>
2023-06-22 13:08:46 -0600

    añadido

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 // Inserte el código aquí
+function actualhour() {
+  let fecha = new Date();
+  let horas = fecha.getHours();
+  let minuto = fecha.getMinutes();
+  let segundo = fecha.getSeconds();
+  let hora = horas + " . " + minuto + " . " + segundo;
 
+  document.getElementById("horaActual").textContent = hora;
+}
+
+setInterval(actualhour, 1000);
+
+// obtener usuarios
+obtenerListaUsuarios();
+
+function obtenerListaUsuarios() {
+  fetch("http://localhost:3000/users")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Lista de usuarios:", data);
+    })
+    .catch((error) => {
+      console.log("Error al obtener la lista de usuarios", error);
+    });
+
+  function usuarios() {
+    return new Promise(obtenerListaUsuarios);
+  }
+}
+
+async function datoUsuario() {
+  const datosdelUsuario = await obtenerListaUsuarios();
+  console.log(datosdelUsuario);
+}
+datosUsuarios();
